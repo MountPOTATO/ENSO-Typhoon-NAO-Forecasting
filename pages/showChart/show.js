ed7d0db5d1f9ca059b533ec739f509a16f30c9ba
NathanHolmes4 <503253721@qq.com>
2021-05-22 09:51:21 +0800

    添加了简单的选择箱

--- pages/showChart/show.js
+++ pages/showChart/show.js
@@ -8,7 +8,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    options: [{
+      city_id: '001',
+      city_name: '北京'
+    }, {
+      city_id: '002',
+      city_name: '上海'
+    }, {
+      city_id: '003',
+      city_name: '深圳'
+    }],
+    selected:{}
+  },
+  change (e) {
+    this.setData({
+      selected: { ...e.detail }
+    })
+    wx.showToast({
+      title: `${this.data.selected.id} - ${this.data.selected.name}`,
+      icon: 'success',
+      duration: 1000
+    })
+  },
+  close () {
+    // 关闭select
+    this.selectComponent('#select').close()
   },
   //获取数据按钮
   getData_btn:function(){
